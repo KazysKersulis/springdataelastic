8a64531e273dd69b88fc51c389f844e11a719b97
Kazys Kersulis <kazimieras.kersulis@gmail.com>
2018-12-03 23:20:57 +0200

    initial commit

---
+++ src/main/java/com/example/springdataelastic/SpringdataelasticApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdataelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataelasticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataelasticApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/springdataelastic/service/BookServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.springdataelastic.service;
+
+import com.example.springdataelastic.model.Book;
+import com.example.springdataelastic.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+
+    @Override
+    public Book findOne(String id) {
+        return bookRepository.findById(id).get();
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Page<Book> findByAuthor(String author, PageRequest pageRequest) {
+        return bookRepository.findByAuthor(author, pageRequest);
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+}
